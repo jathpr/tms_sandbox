4100b3145349aace2afda609ff551d282ace0d45
jathpr <jathpr@gmail.com>
2020-03-26 20:18:18 +0300

    examples

--- examples/hess.js
+++ examples/hess.js
@@ -3,6 +3,7 @@
 //   Хэеса считается так: если ввели число 9, то проверяем его, если оно чётное, делим на 2, если нечетное, умножаем на 3 и прибавляем 1, и так до тех пор пока число не
 //   будет равно 1.
 
+// clear
 const input = 9;
 let num = input;
 
@@ -13,3 +14,25 @@ while (num !== 1) {
     num = num * 3 + 1;
   }
 }
+
+//full
+const start = 9;
+
+let curent = start;
+let steps = 0;
+let maxValue = start;
+while (curent != 1) {
+  if (curent > maxValue) maxValue = curent;
+  if (curent % 2 === 0) {
+    const oldVal = curent;
+    curent = curent / 2;
+    console.log(`step ${steps + 1}: ${oldVal}/2 = ${curent}`);
+  } else {
+    const oldVal = curent;
+    curent = curent * 3 + 1;
+    console.log(`step ${steps + 1}: ${oldVal}*3 + 1 = ${curent}`);
+  }
+  steps++;
+}
+console.log('maxValue', maxValue);
+console.log('steps', steps);
--- test.js
+++ test.js
@@ -1,25 +1,34 @@
-// \* Найти последовательность Хэеса, для числа должно быть выдано сообщение, с самой последовательностью, количеством шагов,
-// которое понадобилось для нахождения этой последовательности, и вершиной последовательности, это максимальное число из этой последовательности. Последовательность
-// Хэеса считается так: если ввели число 9, то проверяем его, если оно чётное, делим на 2, если нечетное, умножаем на 3 и прибавляем 1, и так до тех пор пока число не
-// будет равно 1.
-
-const start = 9;
-
-let curent = start;
-let steps = 0;
-let maxValue = start;
-while (curent != 1) {
-  if (curent > maxValue) maxValue = curent;
-  if (curent % 2 === 0) {
-    const oldVal = curent;
-    curent = curent / 2;
-    console.log(`step ${steps + 1}: ${oldVal}/2 = ${curent}`);
-  } else {
-    const oldVal = curent;
-    curent = curent * 3 + 1;
-    console.log(`step ${steps + 1}: ${oldVal}*3 + 1 = ${curent}`);
-  }
-  steps++;
+'use strict';
+const x = 1;
+console.log(x);
+{
+  const x = 12;
+  console.log(x);
 }
-console.log('maxValue', maxValue);
-console.log('steps', steps);
+console.log(x);
+{
+  // a = 12;
+  // console.log('a', a);
+}
+
+const foo = 7;
+const bar = {
+  foo: 7,
+  bar: 'rt'
+};
+
+edite(foo);
+edite(bar);
+
+function edite(inp) {
+  if (typeof inp !== 'number') inp.foo = 11;
+  else inp = 11;
+
+  console.log('inp', inp);
+}
+console.log('edite -> edite', edite);
+
+console.log('foo', foo);
+console.log('bar', bar);
+
+const show = () => console.log('a');
