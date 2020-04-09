420165b0dd657a81b508c9e41d315c9ee20d6341
Dmitry <jathpr@gmail.com>
2020-04-09 22:50:50 +0300

    hw10

---
+++ HomeWork/lesson10.md
@@ -0,0 +1,14 @@
+### Теория
+
+- дескрипторы/гетеры/сетеры - https://learn.javascript.ru/object-properties
+- прототипы - https://learn.javascript.ru/prototypes
+- классы - https://learn.javascript.ru/class
+- наследование - https://learn.javascript.ru/class-inheritance
+
+### Задания
+
+- таймер - https://learn.javascript.ru/task/rewrite-to-class
+- наследование таймера - https://learn.javascript.ru/task/clock-class-extended
+- Задача из прошлого занятия. Создать классы Круг/Квадрат наследуемые от класса Форма. Подробную инструкцию добавлю позже.  
+  \* Написать крестики нолики используя классы
+  \*\*\* Написать ToDo используя классы (задача масштабов курсовой, украсит любой гитхаб)
--- test.js
+++ test.js
@@ -15,18 +15,13 @@
 // console.log('user', user.toString());
 // console.log('user', '' + user);
 
-function User(name) {
-  this.name = name;
-  this.isAdmin = false;
-}
+// let user = new User('Вася');
 
-let user = new User('Вася');
+// console.log(user.name); // Вася
+// console.log(user.isAdmin); // false
 
-console.log(user.name); // Вася
-console.log(user.isAdmin); // false
-
-const u2 = User('fds');
-console.log('u2', u2);
+// const u2 = User('fds');
+// console.log('u2', u2);
 
 // let num = [5, 4, 6, 9, 8, 1, 2, 10, 3, 7];
 // num.sort(sort);
@@ -67,16 +62,108 @@ console.log('u2', u2);
 // // alert(counter()); // 1
 // // alert(counter()); // 2
 
-// const n = 2;
-// const m = 3;
+// const Shapes = {
+//   circle: 'Circle',
+//   square: 'Square',
+// };
 
-// const arr = new Array(n);
-
-// for (let i = 0; i < arr.length; i++) {
-//   arr[i] = new Array(m);
-//   for (let j = 0; j < arr[i].length; j++) {
-//     arr[i][j] = j;
-//   }
+// function Shape(type, name) {
+//   this.name = name;
+//   this.type = type;
+//   this.sq = function () {
+//     return this.calcSq();
+//   };
 // }
 
-// console.log(arr);
+// function Сircle(name, d) {
+//   const circle = new Shape(Shapes.circle, name);
+//   circle.d = d;
+//   circle.calcSq = function () {
+//     return (this.d / 2) ** 2 * Math.PI;
+//   };
+//   Object.defineProperty(circle, 'square', {
+//     get: function () {
+//       return this.calcSq();
+//     },
+//     enumerable: true,
+//   });
+//   return circle;
+// }
+
+// function Square(name, side) {
+//   const shape = new Shape(Shapes.square, name);
+//   for (const key in shape) {
+//     this[key] = shape[key];
+//   }
+//   this.side = side;
+//   this.calcSq = function () {
+//     return this.side ** 2;
+//   };
+//   Object.defineProperty(this, 'square', {
+//     get: () => {
+//       return this.calcSq();
+//     },
+//     enumerable: true,
+//   });
+// }
+
+// const showShape = (shape) => {
+//   console.log('--------------------');
+//   console.log('showShape -> name', shape.name);
+//   console.log('showShape -> type', shape.type);
+//   console.log('showShape -> square', shape.square);
+//   switch (shape.type) {
+//     case Shapes.circle:
+//       console.log('showShape -> diameter', shape.d);
+//       break;
+//     case Shapes.square:
+//       console.log('showShape -> side', shape.side);
+//       break;
+
+//     default:
+//       break;
+//   }
+// };
+
+// showShape(new Сircle('one', 3));
+// showShape(new Square('two', 3));
+
+// const obj1 = { a: 1 };
+// const obj2 = new Object({ a: 1 });
+// console.log('obj1', obj1);
+// console.log('obj2', obj2);
+
+class Clock {
+  constructor({ template }) {
+    this.template = template;
+  }
+
+  start() {
+    this.render();
+    this.timer = setInterval(() => this.render(), 1000);
+  }
+
+  stop() {
+    clearInterval(this.timer);
+  }
+
+  render() {
+    let date = new Date();
+
+    let hours = date.getHours();
+    if (hours < 10) hours = '0' + hours;
+
+    let mins = date.getMinutes();
+    if (mins < 10) mins = '0' + mins;
+
+    let secs = date.getSeconds();
+    if (secs < 10) secs = '0' + secs;
+
+    let output = this.template.replace('h', hours).replace('m', mins).replace('s', secs);
+
+    console.log(output);
+  }
+}
+
+let clock = new Clock({ template: 'h:m:s' });
+clock.start();
