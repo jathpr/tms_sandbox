c4454555f19b2bab749eebc42e2d823cd9d838ce
Dmitry <jathpr@gmail.com>
2020-04-28 16:26:45 +0300

    dirs

--- test.js
+++ test.js
@@ -1,23 +1,5 @@
 'use strict';
 // const x = require('./expo');
 
-let promise = new Promise(function (resolve, reject) {
-  // setTimeout(() => resolve(1), 500);
-});
-
-function onError(error) {
-  console.log('onError -> error', error);
-}
-
-function onSuccess(data) {
-  console.log('onSuccess -> data', data);
-}
-
-(async () => {
-  let url = 'https://api.github.com/repos/javascript-tutorial/en.javascript.info/commits';
-  let response = await fetch(url);
-
-  let commits = await response.json(); // читаем ответ в формате JSON
-
-  console.log(commits[0].author.login);
-})();
+function isPalindrom(str) {}
+console.log(isPalindrom('А роза упала на лапу Азораe'));
